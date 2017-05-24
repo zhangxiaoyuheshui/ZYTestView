179bd464576a93df8e1f9a356fd91236c85b444d
MAX <zhangyu@ihealthlabs.com.cn>
2017-05-24 14:25:07 +0800

    add project

---
+++ ZYTestView/ZYTestView/ZYTestView.h
@@ -0,0 +1,13 @@
+//
+//  ZYTestView.h
+//  ZYTestView
+//
+//  Created by zhangyu on 2017/5/24.
+//  Copyright © 2017年 zhangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYTestView : UIView
+
+@end
---
+++ ZYTestView/ZYTestView/ZYTestView.m
@@ -0,0 +1,16 @@
+//
+//  ZYTestView.m
+//  ZYTestView
+//
+//  Created by zhangyu on 2017/5/24.
+//  Copyright © 2017年 zhangyu. All rights reserved.
+//
+
+#import "ZYTestView.h"
+
+@implementation ZYTestView
+
+- (UIView *)createView {
+    return [[UIView alloc] init];
+}
+@end
